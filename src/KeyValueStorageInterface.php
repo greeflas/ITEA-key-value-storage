a58ee6d8e901c619545b251a41f2a75a5207865c
greeflas <vldmr.kuprienko@gmail.com>
2018-06-25 16:54:31 +0300

    Creates key value storage interface

---
+++ src/KeyValueStorageInterface.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Greeflas\Storage;
+
+interface KeyValueStorageInterface
+{
+    /**
+     * Store value by key.
+     *
+     * @param string    $key
+     * @param mixed     $value
+     */
+    public function set(string $key, $value): void;
+
+    /**
+     * Gets value by key.
+     *
+     * @param string $key
+     *
+     * @return mixed Can have any type: int, string, null, array, e.g.
+     */
+    public function get(string $key);
+
+    /**
+     * Check whether value is exist by key.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
+    public function has(string $key): bool;
+
+    /**
+     * Removes value by key.
+     *
+     * @param string $key
+     */
+    public function remove(string $key): void;
+
+    /**
+     * Clear storage.
+     */
+    public function clear(): void;
+}
